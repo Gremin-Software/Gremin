9c4a87120b27df715809b6daed5f8c1dd00a8259
Bartosz Żak <bartosz.zak0@gmail.com>
2020-07-16 14:52:30 +0200

    Możliwość zmiany predkosci granicznej i poczatkowej skoku

--- gremin.py
+++ gremin.py
@@ -14,7 +14,7 @@ run = True
 class Player:
     gravity = 1
 
-    def __init__(self, pos_x, pos_y, width, height, image, movement_vel=5):
+    def __init__(self, pos_x, pos_y, width, height, image, movement_vel=5, jump_vel=20, terminal_vel=15):
         self.pos_x = pos_x  # player position
         self.pos_y = pos_y  #
         self.width = width
@@ -25,6 +25,8 @@ class Player:
         self.movement_vel = movement_vel
         self.jumping = False
         self.can_jump = False
+        self.jump_vel = jump_vel
+        self.terminal_vel = terminal_vel
         self.fall_vel = 0
         self.player_rect = pygame.Rect(self.pos_x, self.pos_y, self.width, self.height)  # used in collision_test()
 
@@ -55,8 +57,8 @@ class Player:
 
         movement[1] += self.fall_vel
         self.fall_vel += 1
-        if self.fall_vel > 15:
-            self.fall_vel = 15
+        if self.fall_vel > self.terminal_vel:
+            self.fall_vel = self.terminal_vel
 
         self.pos_x += movement[0]
         collisions = self.collision_test(tiles)
@@ -82,7 +84,7 @@ class Player:
         if collision_types['bottom']:
             self.can_jump = True
         if self.jumping and self.can_jump:
-            self.fall_vel = -20
+            self.fall_vel = -self.jump_vel
             self.can_jump = False
         self.jumping = False
 
@@ -120,8 +122,8 @@ while run:  # main loop
     for tile in tiles:
         pygame.draw.rect(main_window, (0, 255, 0), tile)  # draws the tiles from tiles list every frame
 
-    gremin.draw(main_window)
     gremin.move()
+    gremin.draw(main_window)
 
     pygame.display.update()
     clock.tick(60)  # sets the FPS to 60 :))
